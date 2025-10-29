c66b0a50a3244dd55bcd0ac1c42c437d115f646e
Lee Wang <leeeee@gmail.com>
2025-10-29 16:11:22 -0700

    wip

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,13 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+
+  res.writeHead(200, {
+    'content-type': 'text/html'
+  })
+  console.log('User hit server')
+  res.write('<h1>home page</h1>')
+  res.end()
+})
+
+server.listen(3000)
