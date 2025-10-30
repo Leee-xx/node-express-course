65ab295deb60d494ad44782add8c8c2ab4e29efa
Lee Wang <leeeee@gmail.com>
2025-10-30 13:56:41 -0700

    wip

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,13 +1,28 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+  const url = req.url
 
-  res.writeHead(200, {
-    'content-type': 'text/html'
-  })
-  console.log('User hit server')
-  res.write('<h1>home page</h1>')
-  res.end()
+  if (url === '/') {
+    res.writeHead(200, {
+      'content-type': 'text/html'
+    })
+    res.write('<h1>home page</h1>')
+    res.end()
+  } else if (url === '/about') {
+    res.writeHead(200, {
+      'content-type': 'text/html'
+    })
+    res.write('<h1>about page</h1>')
+    res.end()
+  } else {
+    res.writeHead(404, {
+      'content-type': 'text/html'
+    })
+    res.write('<h1>404 not found</h1>')
+    res.end()
+
+  }
 })
 
 server.listen(3000)
