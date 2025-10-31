3f837f12e4e167169f1ab202ae66149f38f4b998
Lee Wang <leeeee@gmail.com>
2025-10-31 16:22:44 -0700

    adds public/

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,9 +7,11 @@ const port = 3000
 // setup static and middleware
 app.use(express.static('./public'))
 
+/*
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
 })
+*/
 
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
