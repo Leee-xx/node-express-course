c81aa7bceaf88cc853bcc91f2e0f53c870b91126
Lee Wang <leeeee@gmail.com>
2025-11-05 16:25:29 -0800

    middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,44 +1,22 @@
 const express = require('express')
 //const path = require('path')
 const app = express()
-const { products } = require('./data')
+//const { products } = require('./data')
+const logger = require('./logger')
+const authorize = require('./authorize')
 
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/v1/query', (req, res) => {
-  const { search, limit } = req.query
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((p) => {
-      return p.name.startsWith(search)
-    })
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit))
-  }
-
-  if (sortedProducts.length < 1) {
-    return res.status(200).send(({ success: true, data: [] }))
-
-  } else {
-
-  }
-    res.status(200).send(sortedProducts)
-})
-
-/*
-// setup static file's directory and middleware
-app.use(express.static('./public'))
+app.use([authorize, logger])
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+  res.send('home')
 })
-*/
+
+app.get('/about', (req, res) => {
+  console.log(req.user)
+  res.send('about')
+})
 
 app.all('*', (req, res) => {
   res.status(404).send('resource not found')
