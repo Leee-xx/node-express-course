1989a2ea23a07e585ba9aabee821d1b499a27781
Lee Wang <leeeee@gmail.com>
2025-11-13 13:24:04 -0800

    adds routes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+const taskRouter = require('./routes/tasks')
+
+const port = 3000
+
+
+app.get('/hello', (req, res) => {
+  res.send('Task Manager App')
+})
+
+// middleware
+app.use(express.json())
+
+// routes
+app.use('/api/v1/tasks', taskRouter)
+
+app.listen(port, console.log(`Server is listening on port ${port}...`))
