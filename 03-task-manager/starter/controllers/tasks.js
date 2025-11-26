740a046a5da3c94c6b00d039da3058ad47e6e72e
Lee Wang <leeeee@gmail.com>
2025-11-26 14:40:44 -0800

    hooking up db queries

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,16 +1,36 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-  res.send('all items!')
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body)
-  res.status(201).json({ task })
+  try {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id })
+const getTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const task = await Task.findOne({ _id: taskId })
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskId}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 const updateTask = (req, res) => {
   res.send('update task')
