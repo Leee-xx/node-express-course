3037cec6f64952966366a63001d946eeb057d6e2
Lee Wang <leeeee@gmail.com>
2025-11-28 14:45:44 -0800

    adds update and delete endpoints

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -23,6 +23,7 @@ app.get('/hello', (req, res) => {
 
 // middleware
 app.use(express.json())
+app.use(express.static('./public'))
 
 // routes
 app.use('/api/v1/tasks', taskRouter)
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -32,11 +32,34 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error })
   }
 }
-const updateTask = (req, res) => {
-  res.send('update task')
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const data = req.body
+    const task = await Task.findOneAndUpdate({ _id: taskId }, data, { new: true, runValidators: true })
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskId}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskId } = req.params
+    const task = await Task.findOneAndDelete({ _id: taskId })
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id ${taskId}` })
+    }
+
+    res.status(200).json({ task })
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 module.exports = {
   getAllTasks,
