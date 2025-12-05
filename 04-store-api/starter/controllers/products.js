19f2711079402ce1fef4a4393ae3504932239082
Lee Wang <leeeee@gmail.com>
2025-12-05 13:22:38 -0800

    adds query params

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,12 +1,34 @@
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({})
-  res.status(200).json({ products })
+  const { featured, company, name } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = {
+      $regex: name,
+      $options: 'i',
+    }
+  }
+
+  const products = await Product.find(queryObject)
+  res.status(200).json({ products, count: products.length })
 }
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({ })
+  const search = 'ab'
+  const products = await Product.find({
+    name: {
+      $regex: search,
+      $options: 'i'
+    }
+  })
   res.status(200).json({ products, count: products.length })
   //res.status(200).json({msg: 'products testing static' })
 }
