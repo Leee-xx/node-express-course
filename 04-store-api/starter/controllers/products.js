e0472e50f735995501d6d86c3d2cf2ec4d4b44a5
Lee Wang <leeeee@gmail.com>
2025-12-09 15:23:36 -0800

    implements sort, fields, pagination

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product')
 
+function reformatQueryStr(str) {
+  return str.split(',').join(' ')
+}
+
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort, fields } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -17,7 +21,27 @@ const getAllProducts = async (req, res) => {
     }
   }
 
-  const products = await product.find(queryobject)
+  let result = Product.find(queryObject)
+
+  if (sort) {
+    const sortList = reformatQueryStr(sort)
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+  result = result.skip(skip).limit(limit)
+
+  if (fields) {
+    const fieldsList = reformatQueryStr(fields)
+    result = result.select(fieldsList)
+  }
+
+  const products = await result
+
   res.status(200).json({ products, count: products.length })
 }
 
