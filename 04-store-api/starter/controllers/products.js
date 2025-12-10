42ce6aa82f4c4bab5110155ffaf1ec01e6f9a52e
Lee Wang <leeeee@gmail.com>
2025-12-10 14:11:01 -0800

    adds numeric filter

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -5,7 +5,7 @@ function reformatQueryStr(str) {
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query
+  const { featured, company, name, sort, fields, numericFilters } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -20,6 +20,31 @@ const getAllProducts = async (req, res) => {
       $options: 'i',
     }
   }
+
+  if (numericFilters) {
+    const operatorMap = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const regex = /\b(<|<=|=|>|>=)\b/g
+
+    let filters = numericFilters.replace(regex, (match) => `-${operatorMap[match]}-`)
+    const options = ['price', 'rating']
+    filters = filters.split(',').forEach((item) => {
+      const [field, operator, value] = item.split('-')
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) }
+      }
+    })
+
+    console.log(queryObject)
+
+    console.log(numericFilters)
+  }
+
 
   let result = Product.find(queryObject)
 
