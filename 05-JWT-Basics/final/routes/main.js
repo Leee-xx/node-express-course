ea31d92eb80fe1b14d3da82b03150d0bdacc3a91
Lee Wang <leeeee@gmail.com>
2025-12-17 16:28:58 -0800

    wip

--- 05-JWT-Basics/final/routes/main.js
+++ 05-JWT-Basics/final/routes/main.js
@@ -4,7 +4,6 @@ const router = express.Router()
 const { login, dashboard } = require('../controllers/main')
 
 const authMiddleware = require('../middleware/auth')
-
 router.route('/dashboard').get(authMiddleware, dashboard)
 router.route('/login').post(login)
 
--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const jwt = require('jsonwebtoken')
 
 const CustomAPIError = require('../errors/custom-error')
@@ -9,18 +10,43 @@ const login = async (req, res) => {
     throw new CustomAPIError('Please provide username and password', 400)
   }
 
+  // placeholder ID
+  const id = new Date().getDate()
+
+  const token = jwt.sign(
+    { id, username },
+    process.env.JWT_SECRET,
+    { expiresIn: '30d' }
+  )
+
   // mongo
 
   console.log(username, password)
-  res.send('login placeholder')
+  res.status(200).json({ msg: 'user logged in', token })
 }
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100)
-  res.status(200).json({
-    msg: `Hello John Doe`,
-    secret: `placeholder authorized data, lucky number is ${luckyNumber}`
-  })
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    throw new CustomAPIError('No auth token provided', 401)
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+    const luckyNumber = Math.floor(Math.random() * 100)
+    res.status(200).json({
+      msg: `Hello ${decoded.username}`,
+      secret: `placeholder authorized data, lucky number is ${luckyNumber}`
+    })
+
+  } catch (error) {
+    throw new CustomAPIError('Not authorized to access this route', 401)
+  }
+
 }
 
 module.exports = { login, dashboard }
