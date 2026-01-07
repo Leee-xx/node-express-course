44bcd0201fd532b74d4221b2182b9431abb8bbf6
Lee Wang <leeeee@gmail.com>
2026-01-06 16:58:07 -0800

    Adds connectDB (uses 06-JOBS-API as db name)

--- 06-jobs-api/starter/app.js
+++ 06-jobs-api/starter/app.js
@@ -3,7 +3,8 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
-// connextDB
+// connectDB
+const connectDB = require('./db/connect')
 
 // routers
 const authRouter = require('./routes/auth')
@@ -27,6 +28,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
