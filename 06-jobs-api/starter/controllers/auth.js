7c9cba88da410e8b1553ac04970f8be9be3a9bd5
Lee Wang <leeeee@gmail.com>
2026-01-30 13:12:41 -0800

    wip

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,15 +1,39 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const user = await User.create({...req.body})
-  res.status(StatusCodes.CREATED).json({ user })
+  const token = user.createJWT()
+
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      name: user.name,
+    },
+    token
+  })
 }
 
 const login = async (req, res) => {
-  res.send('login user')
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new BadRequestError('Please provide email and password')
+  }
+
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new UnauthenticatedError('Invalid email')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError('Invalid password')
+  }
+
+  const token = user.createJWT()
+  res.status(StatusCodes.OK).json({ user: { name: user.name }, token })
 }
 
 module.exports = {
