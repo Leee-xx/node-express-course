7907e64a7e08e61b996a08abec9d48c54b36833e
Lee Wang <leeeee@gmail.com>
2026-02-11 19:02:15 -0800

    add some custom error handling

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,21 @@
 const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    message: err.message || 'Something went wrong, try again later',
+  }
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+
+  if (err.code && err.code === 11000) {
+    console.log(err)
+    customError.message = `Duplicate value entered for ${Object.keys(err.keyValue)} field(s), please use another value`
+    customError.statusCode = 400
+  }
+
+  return res.status(customError.statusCode).json({ msg: customError.message })
 }
 
 module.exports = errorHandlerMiddleware
