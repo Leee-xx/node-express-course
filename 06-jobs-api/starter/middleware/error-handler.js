56ce0c0cb6a1d8df10e858a1644fefc5ac819a04
Lee Wang <leeeee@gmail.com>
2026-02-13 15:26:18 -0800

    removes CustomApiHandler from middleware

--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,12 +1,8 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || 'Something went wrong, try again later',
-  }
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
   }
 
   if (err.code && err.code === 11000) {
